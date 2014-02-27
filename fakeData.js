ad683987e1e6482beb42de4cf3894a59b10e4525
theInternauts <cricketw@wearetheinternauts.com>
2014-02-26 16:21:04 -0800

    forgot to commit the dummy data

---
+++ fakeData.js
@@ -0,0 +1,201 @@
+
+	var STUBDATA = {
+	  "_total": 5,
+	  "_links": {
+	    "self": "https://api.twitch.tv/kraken/search/streams?limit=25&offset=0&q=starcraft",
+	    "next": "https://api.twitch.tv/kraken/search/streams?limit=25&offset=25&q=starcraft"
+	  },
+	  "streams": [
+	    {
+	      "game": "StarCraft II: Heart of the Swarm",
+	      "viewers": 2,
+	      "_id": 4989654544,
+	      "channel": {
+	        "game": "StarCraft II: Heart of the Swarm",
+	        "display_name": "Blade55555",
+	        "created_at": "2009-09-19T02:07:23Z",
+	        "status": "Starcraft 2 HOTS GM zerg",
+	        "updated_at": "2013-02-28T22:30:17Z",
+	        "mature": null,
+	        "_id": 8341905,
+	        "video_banner": "http://static-cdn.jtvnw.net/jtv_user_pictures/blade55555-channel_offline_image-3abeffa573a391d4-640x360.jpeg",
+	        "name": "blade55555",
+	        "banner": null,
+	        "background": null,
+	        "_links": {
+	          "editors": "https://api.twitch.tv/kraken/channels/blade55555/editors",
+	          "stream_key": "https://api.twitch.tv/kraken/channels/blade55555/stream_key",
+	          "commercial": "https://api.twitch.tv/kraken/channels/blade55555/commercial",
+	          "features": "https://api.twitch.tv/kraken/channels/blade55555/features",
+	          "videos": "https://api.twitch.tv/kraken/channels/blade55555/videos",
+	          "subscriptions": "https://api.twitch.tv/kraken/channels/blade55555/subscriptions",
+	          "follows": "https://api.twitch.tv/kraken/channels/blade55555/follows",
+	          "self": "https://api.twitch.tv/kraken/channels/blade55555",
+	          "chat": "https://api.twitch.tv/kraken/chat/blade55555"
+	        },
+	        "teams": [ ],
+	        "logo": "http://static-cdn.jtvnw.net/jtv_user_pictures/blade55555-profile_image-e551610be15ec896-300x300.png",
+	        "url": "http://www.twitch.tv/blade55555"
+	      },
+	      "preview": "http://static-cdn.jtvnw.net/previews-ttv/live_user_blade55555-320x200.jpg",
+	      "name": "live_user_blade55555",
+	      "_links": {
+	        "self": "https://api.twitch.tv/kraken/streams/blade55555"
+	      },
+	      "broadcaster": "fme"
+	    },
+	    {
+	      "game": "StarCraft II: Heart of the Swarm",
+	      "viewers": 2,
+	      "_id": 4989654544,
+	      "channel": {
+	        "game": "StarCraft II: Heart of the Swarm",
+	        "display_name": "Blade55555",
+	        "created_at": "2009-09-19T02:07:23Z",
+	        "status": "Starcraft 2 HOTS GM zerg",
+	        "updated_at": "2013-02-28T22:30:17Z",
+	        "mature": null,
+	        "_id": 8341905,
+	        "video_banner": "http://static-cdn.jtvnw.net/jtv_user_pictures/blade55555-channel_offline_image-3abeffa573a391d4-640x360.jpeg",
+	        "name": "blade55555",
+	        "banner": null,
+	        "background": null,
+	        "_links": {
+	          "editors": "https://api.twitch.tv/kraken/channels/blade55555/editors",
+	          "stream_key": "https://api.twitch.tv/kraken/channels/blade55555/stream_key",
+	          "commercial": "https://api.twitch.tv/kraken/channels/blade55555/commercial",
+	          "features": "https://api.twitch.tv/kraken/channels/blade55555/features",
+	          "videos": "https://api.twitch.tv/kraken/channels/blade55555/videos",
+	          "subscriptions": "https://api.twitch.tv/kraken/channels/blade55555/subscriptions",
+	          "follows": "https://api.twitch.tv/kraken/channels/blade55555/follows",
+	          "self": "https://api.twitch.tv/kraken/channels/blade55555",
+	          "chat": "https://api.twitch.tv/kraken/chat/blade55555"
+	        },
+	        "teams": [ ],
+	        "logo": "http://static-cdn.jtvnw.net/jtv_user_pictures/blade55555-profile_image-e551610be15ec896-300x300.png",
+	        "url": "http://www.twitch.tv/blade55555"
+	      },
+	      "preview": "http://static-cdn.jtvnw.net/previews-ttv/live_user_blade55555-320x200.jpg",
+	      "name": "live_user_blade55555",
+	      "_links": {
+	        "self": "https://api.twitch.tv/kraken/streams/blade55555"
+	      },
+	      "broadcaster": "fme"
+	    },
+	    {
+	      "game": "StarCraft II: Heart of the Swarm",
+	      "viewers": 2,
+	      "_id": 4989654544,
+	      "channel": {
+	        "game": "StarCraft II: Heart of the Swarm",
+	        "display_name": "Blade55555",
+	        "created_at": "2009-09-19T02:07:23Z",
+	        "status": "Starcraft 2 HOTS GM zerg",
+	        "updated_at": "2013-02-28T22:30:17Z",
+	        "mature": null,
+	        "_id": 8341905,
+	        "video_banner": "http://static-cdn.jtvnw.net/jtv_user_pictures/blade55555-channel_offline_image-3abeffa573a391d4-640x360.jpeg",
+	        "name": "blade55555",
+	        "banner": null,
+	        "background": null,
+	        "_links": {
+	          "editors": "https://api.twitch.tv/kraken/channels/blade55555/editors",
+	          "stream_key": "https://api.twitch.tv/kraken/channels/blade55555/stream_key",
+	          "commercial": "https://api.twitch.tv/kraken/channels/blade55555/commercial",
+	          "features": "https://api.twitch.tv/kraken/channels/blade55555/features",
+	          "videos": "https://api.twitch.tv/kraken/channels/blade55555/videos",
+	          "subscriptions": "https://api.twitch.tv/kraken/channels/blade55555/subscriptions",
+	          "follows": "https://api.twitch.tv/kraken/channels/blade55555/follows",
+	          "self": "https://api.twitch.tv/kraken/channels/blade55555",
+	          "chat": "https://api.twitch.tv/kraken/chat/blade55555"
+	        },
+	        "teams": [ ],
+	        "logo": "http://static-cdn.jtvnw.net/jtv_user_pictures/blade55555-profile_image-e551610be15ec896-300x300.png",
+	        "url": "http://www.twitch.tv/blade55555"
+	      },
+	      "preview": "http://static-cdn.jtvnw.net/previews-ttv/live_user_blade55555-320x200.jpg",
+	      "name": "live_user_blade55555",
+	      "_links": {
+	        "self": "https://api.twitch.tv/kraken/streams/blade55555"
+	      },
+	      "broadcaster": "fme"
+	    },
+	    {
+	      "game": "StarCraft II: Heart of the Swarm",
+	      "viewers": 2,
+	      "_id": 4989654544,
+	      "channel": {
+	        "game": "StarCraft II: Heart of the Swarm",
+	        "display_name": "Blade55555",
+	        "created_at": "2009-09-19T02:07:23Z",
+	        "status": "Starcraft 2 HOTS GM zerg",
+	        "updated_at": "2013-02-28T22:30:17Z",
+	        "mature": null,
+	        "_id": 8341905,
+	        "video_banner": "http://static-cdn.jtvnw.net/jtv_user_pictures/blade55555-channel_offline_image-3abeffa573a391d4-640x360.jpeg",
+	        "name": "blade55555",
+	        "banner": null,
+	        "background": null,
+	        "_links": {
+	          "editors": "https://api.twitch.tv/kraken/channels/blade55555/editors",
+	          "stream_key": "https://api.twitch.tv/kraken/channels/blade55555/stream_key",
+	          "commercial": "https://api.twitch.tv/kraken/channels/blade55555/commercial",
+	          "features": "https://api.twitch.tv/kraken/channels/blade55555/features",
+	          "videos": "https://api.twitch.tv/kraken/channels/blade55555/videos",
+	          "subscriptions": "https://api.twitch.tv/kraken/channels/blade55555/subscriptions",
+	          "follows": "https://api.twitch.tv/kraken/channels/blade55555/follows",
+	          "self": "https://api.twitch.tv/kraken/channels/blade55555",
+	          "chat": "https://api.twitch.tv/kraken/chat/blade55555"
+	        },
+	        "teams": [ ],
+	        "logo": "http://static-cdn.jtvnw.net/jtv_user_pictures/blade55555-profile_image-e551610be15ec896-300x300.png",
+	        "url": "http://www.twitch.tv/blade55555"
+	      },
+	      "preview": "http://static-cdn.jtvnw.net/previews-ttv/live_user_blade55555-320x200.jpg",
+	      "name": "live_user_blade55555",
+	      "_links": {
+	        "self": "https://api.twitch.tv/kraken/streams/blade55555"
+	      },
+	      "broadcaster": "fme"
+	    },
+	    {
+	      "game": "StarCraft II: Heart of the Swarm",
+	      "viewers": 2,
+	      "_id": 4989654544,
+	      "channel": {
+	        "game": "StarCraft II: Heart of the Swarm",
+	        "display_name": "Blade55555",
+	        "created_at": "2009-09-19T02:07:23Z",
+	        "status": "Starcraft 2 HOTS GM zerg",
+	        "updated_at": "2013-02-28T22:30:17Z",
+	        "mature": null,
+	        "_id": 8341905,
+	        "video_banner": "http://static-cdn.jtvnw.net/jtv_user_pictures/blade55555-channel_offline_image-3abeffa573a391d4-640x360.jpeg",
+	        "name": "blade55555",
+	        "banner": null,
+	        "background": null,
+	        "_links": {
+	          "editors": "https://api.twitch.tv/kraken/channels/blade55555/editors",
+	          "stream_key": "https://api.twitch.tv/kraken/channels/blade55555/stream_key",
+	          "commercial": "https://api.twitch.tv/kraken/channels/blade55555/commercial",
+	          "features": "https://api.twitch.tv/kraken/channels/blade55555/features",
+	          "videos": "https://api.twitch.tv/kraken/channels/blade55555/videos",
+	          "subscriptions": "https://api.twitch.tv/kraken/channels/blade55555/subscriptions",
+	          "follows": "https://api.twitch.tv/kraken/channels/blade55555/follows",
+	          "self": "https://api.twitch.tv/kraken/channels/blade55555",
+	          "chat": "https://api.twitch.tv/kraken/chat/blade55555"
+	        },
+	        "teams": [ ],
+	        "logo": "http://static-cdn.jtvnw.net/jtv_user_pictures/blade55555-profile_image-e551610be15ec896-300x300.png",
+	        "url": "http://www.twitch.tv/blade55555"
+	      },
+	      "preview": "http://static-cdn.jtvnw.net/previews-ttv/live_user_blade55555-320x200.jpg",
+	      "name": "live_user_blade55555",
+	      "_links": {
+	        "self": "https://api.twitch.tv/kraken/streams/blade55555"
+	      },
+	      "broadcaster": "fme"
+	    }
+
+	  ]
+	}
