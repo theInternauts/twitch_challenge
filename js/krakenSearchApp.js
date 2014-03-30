20e9b433748265cb2e0042c1bbea2aea0f42df84
theInternauts <cricketw@WeAreTheInternauts.com>
2014-03-30 03:25:22 -0700

    added a lot of notes marking my conversion progress

--- js/krakenSearchApp.js
+++ js/krakenSearchApp.js
@@ -9,7 +9,7 @@ function KrakenSearchApp(id, options){
 
 		this.init()
 	}
-
+// DONE // controller
 	KrakenSearchApp.prototype.paginationClickHandler = function(event){
 		event.preventDefault()
 		switch(event.target.id){
@@ -28,7 +28,7 @@ function KrakenSearchApp(id, options){
 		}
 		this.searchAPI()
 	}
-
+// DONE // controller
 	KrakenSearchApp.prototype.maxNumberOfPages = function(){
 		var num = Math.floor(this.totalResults / this.resultsPerPage)
 		if (this.totalResults % this.resultsPerPage > 0){
@@ -37,12 +37,12 @@ function KrakenSearchApp(id, options){
 			return num
 		}
 	}
-
+// DONE // resultsPaginationControlView
 	KrakenSearchApp.prototype.updatePaginationDisplay = function(){
 		var display = document.getElementById('display-pg')
 		display.innerHTML = this.currentPage + ' / ' + this.maxNumberOfPages()
 	}
-
+// DONE // resultsPaginationControlsView
 	KrakenSearchApp.prototype.buildResultsPageControlsTemplate = function(parentId){
 		var textFragment = '<a href="#" id="btn-pg-first">&#8647;</a><a href="#" id="btn-pg-previous">&#8678;</a><span id="display-pg">1 / 1</span><a href="#" id="btn-pg-next">&#8680;</a><a href="#" id="btn-pg-last">&#8649;</a>'
 		var parent = document.getElementById(parentId)
@@ -54,7 +54,7 @@ function KrakenSearchApp(id, options){
 		pageControls.addEventListener('click', this.paginationClickHandler.bind(this), false)
 		this.updatePaginationDisplay()			
 	}
-
+// DONE // controller
 	KrakenSearchApp.prototype.buildQueryString = function(){
 		var limit = '?limit=' + encodeURI(this.resultsPerPage)
 		var offset = '&offset=' + encodeURI(((this.currentPage-1)*this.resultsPerPage))
@@ -64,14 +64,15 @@ function KrakenSearchApp(id, options){
 		console.log(this.queryString)
 	}
 
-		
 
+// DONE // controller
 	KrakenSearchApp.prototype.init = function(){
 		console.log("init")
 		this.buildSearchBar()
 		this.buildResultsPanelContainer()
 	}
-	
+
+// DONE // searchBarPanelView
 	KrakenSearchApp.prototype.buildSearchBar = function(){
 		var textFragment = '<input id="input-search" type="text" placeholder="Search query..." /><input type="submit" value="Search" id="btn-input-search-button"/>'		
 		var HTMLfragment = this.UtilCreateFragment('DIV', 'search-panel', 'panel')
@@ -80,17 +81,17 @@ function KrakenSearchApp(id, options){
 
 		this.setSearchListeners()
 	}
-
+// DONE // searchBarPanelView
 	KrakenSearchApp.prototype.setSearchListeners = function(){
 		document.getElementById('input-search').addEventListener('keyup', this.searchHandler.bind(this), true)
 		document.getElementById('btn-input-search-button').addEventListener('click', this.searchAPI.bind(this), true)
 	}
-
+// DONE // controller
 	KrakenSearchApp.prototype.searchHandler = function(event){
 		this.searchTerm = event.target.value
 		this.searchAPI()	
 	}
-
+// DONE // controller
 	KrakenSearchApp.prototype.searchAPI = function(term){
 		(term != null && typeof(term) == 'string') ? this.searchTerm = term : null
 		this.buildQueryString()
@@ -104,7 +105,7 @@ function KrakenSearchApp(id, options){
 		
 		server.loadJSONP(this.queryString, config)
 	}
-
+// DONE // resultsListView
 	KrakenSearchApp.prototype.displaySearchResults = function(data, parentId){
 		this.totalResults = data._total
 		this.updateResultsCount(this.totalResults)
@@ -120,12 +121,13 @@ function KrakenSearchApp(id, options){
 			parent.appendChild(HTMLfragment)
 		})		
 	}
-
+// DONE // resultsCountView
 	KrakenSearchApp.prototype.updateResultsCount = function(newCount){
 		var counterDisplayNode = document.getElementById('results-count').childNodes[1]
 		counterDisplayNode.innerHTML = newCount
 	}
 
+// DONE // pull out into Util Class
 	KrakenSearchApp.prototype.UtilCreateFragment = function(elementTag, nodeId, nodeClass){
 		//currently this only supports setting an ID attribute and/or a single class attribute
 		var HTMLfragment = document.createDocumentFragment();
@@ -144,12 +146,14 @@ function KrakenSearchApp(id, options){
 		return HTMLfragment
 	}
 
+// DONE // pull out into Util Class
 	KrakenSearchApp.prototype.UtilEmptyNode = function(node){
 		while (node.hasChildNodes()) {
 		    node.removeChild(node.lastChild);
 		}
 	}
 
+// DONE //
 	KrakenSearchApp.prototype.buildResultsPanelContainer = function(){	
 		var HTMLfragment = this.UtilCreateFragment('DIV', 'results-panel', 'panel')			
 		this.root.appendChild(HTMLfragment)
@@ -158,6 +162,7 @@ function KrakenSearchApp(id, options){
 		this.buildResultsListTemplate('results-panel')
 	}
 
+// DONE // results view
 	KrakenSearchApp.prototype.buildResultsCountTemplate = function(parentId){
 		var textFragment = '<span>Total results: </span><span>0</span>'		
 		var parent = document.getElementById(parentId)		
@@ -166,12 +171,14 @@ function KrakenSearchApp(id, options){
 		parent.appendChild(HTMLfragment)
 	}
 
+// DONE // results view
 	KrakenSearchApp.prototype.buildResultsListTemplate = function(parentId){
 		var parent = document.getElementById(parentId)
 		var HTMLfragment = this.UtilCreateFragment('UL', 'results-list')
 		parent.appendChild(HTMLfragment)
 	}
 
+// DONE // controller
 	KrakenSearchApp.prototype.AsyncErrorHandler = function(errorMessage){
 		var HTMLfragment = this.UtilCreateFragment('DIV', 'msg-async', 'msg-error')
 		var newNode = HTMLfragment.childNodes[0]
